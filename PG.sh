01cb08579ac2b823f7dcd5c2ce8498a74eedafa6
jixt <jixt@f582c0bb-8d2e-0410-8fae-e3a1772be7bf>
2008-08-22 14:32:53 +0000

    Added Begasus as author of the PG.sh script
    git-svn-id: http://svn.osdrawer.net/photograbber@49 f582c0bb-8d2e-0410-8fae-e3a1772be7bf

--- PG.sh
+++ PG.sh
@@ -1,5 +1,6 @@
 ## *****************************************************************
-## * Copyright (c) 2004-2008,	Jan-Rixt Van Hoye				   *
+## * Copyright (c) 2004-2008, Jan-Rixt Van Hoye aka'Jixt'		   *
+##,* 							and Luc Schrijvers aka 'Begasus'   *
 ## * All rights reserved.										   *
 ## * Distributed under the terms of the MIT License.               *
 ## *****************************************************************
