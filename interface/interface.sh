5c0e79e4eabfde0a1d99916c91ef44c71eb86d4e
jixt <jixt@f582c0bb-8d2e-0410-8fae-e3a1772be7bf>
2008-08-28 13:33:53 +0000

    Added interface build script
    git-svn-id: http://svn.osdrawer.net/photograbber@61 f582c0bb-8d2e-0410-8fae-e3a1772be7bf

---
+++ interface/interface.sh
@@ -0,0 +1,16 @@
+## *****************************************************************
+## * Copyright (c) 2004-2008, Jan-Rixt Van Hoye aka'Jixt'		   *
+##,* 							and Luc Schrijvers aka 'Begasus'   *
+## * All rights reserved.										   *
+## * Distributed under the terms of the MIT License.               *
+## *****************************************************************
+
+## Buildfile for PhotoGrabber Interface
+
+# Cleaning the build system
+#make clean
+# Build the interface
+make
+# Copy the binaries to the build directory
+cp ./obj.x86/libPGInterface ../build/PhotoGrabber/interface/
+
