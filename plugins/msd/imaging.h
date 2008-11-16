62c94d7b8417156341700aaa2f21f1efbfa7d5e5
jixt <jixt@f582c0bb-8d2e-0410-8fae-e3a1772be7bf>
2008-11-16 10:56:09 +0000

    Added scaling source files for the mass storage plugin
    git-svn-id: http://svn.osdrawer.net/photograbber@91 f582c0bb-8d2e-0410-8fae-e3a1772be7bf

---
+++ plugins/msd/imaging.h
@@ -0,0 +1,12 @@
+/*****************************************************************
+ * Copyright (c) 2004-2008,	Jan-Rixt Van Hoye					 *
+ * All rights reserved.											 *
+ * Distributed under the terms of the MIT License.               *
+ *****************************************************************/
+
+typedef long status_t;
+typedef unsigned long uint32;
+
+enum scale_method {
+    IMG_SCALE_BILINEAR = 1
+};
